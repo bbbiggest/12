f75169efd44d8c7c5143223699eb22ce6b4709f5
bbbiggest <60065681+bbbiggest@users.noreply.github.com>
2020-10-03 21:23:16 +0800

    Add files via upload

---
+++ CodeForces/1409A-YetAnotherTwoIntegersProblem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        long long a,b;
+        cin >> a >> b;
+        long long sum;
+        int x;
+        sum = a>b?a-b:b-a;
+        if (sum == 0)
+            cout << "0" << endl;
+        else
+        {
+            x = 1-!(sum%10);
+            cout << sum/10+x << endl;
+        }
+    }
+    return 0;
+}
