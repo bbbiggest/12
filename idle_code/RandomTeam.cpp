f8408d1f8d728c3787f7cd419982fc4e4b00d3da
bbbiggest <60065681+bbbiggest@users.noreply.github.com>
2021-02-16 00:53:59 +0800

    Update RandomTeam.cpp

--- idle_code/RandomTeam.cpp
+++ idle_code/RandomTeam.cpp
@@ -17,7 +17,7 @@ int main()
     cout << "Enter all the people who need to team up:\n";
     // 输入所有需组队的人
     cout << "Whether you need to read from file \"member.txt\" ?\nY or N\n";
-    // 是否需要从文件"member.txt"中读取
+    // 是否需要从文件"member.txt"中读取(需与本文件在同一根目录下)
     char c;
     bool isopen = false;
     while (cin >> c)
@@ -59,34 +59,35 @@ int main()
     if (isopen)
         freopen("CON", "r", stdin);
     random_shuffle(names.begin(), names.end());
-    int number = names.size();
+    int cnt = names.size();
 
     time_t now = time(0);
-    char date[100];
+    char date[100], fname[100];
     strftime(date, sizeof(date), "%Y/%m/%d-team_results", localtime(&now));
+    strftime(fname, sizeof(fname), "%Y_%m_%d-team_results.txt", localtime(&now));
 
     int teamnum = 0;
     cout.flush();
-    freopen("team_results.txt", "w", stdout);
+    freopen(fname, "w", stdout);
     cout << date << "\n";
     cout << "Team " << ++teamnum << ": ";
-    for (int i = 0; i < number; ++i)
+    for (int i = 0; i < cnt; ++i)
     {
         cout << setw(15) << left << names[i] << "\t";
-        if ((i + 1) % 3 == 0 && (i + 1) != number)
+        if ((i + 1) % 3 == 0 && (i + 1) != cnt)
             cout << "\nTeam " << ++teamnum << ": ";
     }
     cout.flush();
     freopen("CON", "w", stdout);
-    cout << "The result has been saved to \"team_results.txt\"\n";
+    cout << "The result has been saved to \"" << fname << "\"\n";
     // 将结果储存至"team_results.txt"，同时也在屏幕上显示
     cout << "The team grouping results are as follows: \n";
     teamnum = 0;
     cout << "Team " << ++teamnum << ": ";
-    for (int i = 0; i < number; ++i)
+    for (int i = 0; i < cnt; ++i)
     {
         cout << setw(15) << left << names[i] << "\t";
-        if ((i + 1) % 3 == 0 && (i + 1) != number)
+        if ((i + 1) % 3 == 0 && (i + 1) != cnt)
             cout << "\nTeam " << ++teamnum << ": ";
     }
     cout.flush();
